b834fa3576dd1616b48dc85d98f7cd813611212b
sschulte <sschulte@bell.bank>
2024-10-22 15:47:19 -0500

    Add project files.

---
+++ BethanysPieShop/Program.cs
@@ -0,0 +1,33 @@
+using BethanysPieShop.Models;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+
+builder.Services.AddScoped<IPieRepository, PieRepository>();
+
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<BethanysPieShopDbContext>(options =>
+{
+	options.UseSqlServer(
+		builder.Configuration["ConnectionStrings:BethanysPieShopDbContextConnection"]);
+});
+
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+
+if (app.Environment.IsDevelopment())
+{
+	app.UseDeveloperExceptionPage();
+}
+
+app.MapDefaultControllerRoute(); //"{controller=Home}/{action=Index}/{id?}"
+
+
+DbInitializer.Seed(app);
+
+app.Run();
